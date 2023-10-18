8b997ccd85c30b887cfb4b0dae5551398af9e904
AC_Marvel <gil_1_10@hotmail.com>
2023-10-18 21:30:35 +0200

    Depois de descobrir as cartas todas, recebe uma mengaem de "Parabens, você ganhou". Conclusão da craição do jogo...

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -25,24 +25,34 @@ const createElement = (tag, className) => {
 let firstCard = '';
 let secondCard = '';
 
+const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll('.disabled-card');
+
+    if (disabledCards.length == 20) {
+        alert('Parabens, você conseguiu!');
+    }
+}
+
 const checkCards = () => {
     const firstCharacter = firstCard.getAttribute('data-character');
     const secondCharacter = secondCard.getAttribute('data-character');
 
     if (firstCharacter == secondCharacter) {
 
-        firstCard.firsChild.classList.add('disabled-card');
-        secondCard.firsChild.classList.add('disabled-card');
+        firstCard.firstChild.classList.add('disabled-card');
+        secondCard.firstChild.classList.add('disabled-card');
         
         firstCard = '';
         secondCard = '';
+
+        checkEndGame();
 
     } else {
         setTimeout(() => {
 
         firstCard.classList.remove('reveal-card');
         secondCard.classList.remove('reveal-card');
-
+        
         firstCard = '';
         secondCard = '';
 
@@ -57,10 +67,12 @@ const revealCard = ({ target }) => {
     }
 
     if (firstCard == '') {
+
         target.parentNode.classList.add('reveal-card');
         firstCard = target.parentNode;
     
     } else if (secondCard == '') {
+
         target.parentNode.classList.add('reveal-card');
         secondCard = target.parentNode;
 
